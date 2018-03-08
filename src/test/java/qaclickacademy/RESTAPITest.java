86f8f2095848542a33be847413e7dfb1c5c14472
rahul <rahulonlinetutor@gmail.com>
2018-03-08 11:43:58 -0500

    fd

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -14,6 +14,10 @@ public class RESTAPITest {
 	public void postJira2()
 	{
 		System.out.println("postJira");
+		if( 1==3)
+		{
+		System.out.println("postJira");
+		}
 	}
 	@Test
 	public void deleteTwitters()
@@ -28,11 +32,30 @@ public class RESTAPITest {
 	@Test
 	public void postJira1()
 	{
+		if( 2==6)
+	{
+	System.out.println("postJira");
+	}
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");	
+		if( 1==3)
+		{
+		System.out.println("postJira");
+		}
+		System.out.println("postJira");
+		System.out.println("deleteTwitter");	
+		if( 1==3)
+		{
+		System.out.println("postJira");
+		}
+		System.out.println("postJira");
 	}
 	@Test
 	public void deleteTwitter()
+	{if( 1==3)
 	{
+	System.out.println("postJira");
+	}
 		//One two
 		//three
 	}
